70ccf9ec8084de9aeb7e2d1a83a8759af536a869
RueJulse <1449747391@qq.com>
2023-07-14 15:43:08 +0800

    client

--- code/client/imageprovider.cpp
+++
@@ -1,9 +0,0 @@
-#include "imageprovider.h"
-
-
-
-QPixmap ImageProvider::requestPixmap(const QString &id, QSize *size, const QSize &requestedSize)
-{
-    if(m_pixmap.count(id)>0)
-        return m_pixmap[id];
-}
